7cb8291919b3a787b7d38806081dbd2902252e94
priyanka-saini8 <63971975+priyanka-saini8@users.noreply.github.com>
2023-06-08 22:44:56 +0530

    added service CRUD

--- employeeCRUDApp/src/main/java/com/example/employeeCRUDApp/service/EmployeeService.java
+++ employeeCRUDApp/src/main/java/com/example/employeeCRUDApp/service/EmployeeService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface EmployeeService {
 
     List<Employee> findAl();
+
+    Employee findById(int theId);
+
+    Employee save(Employee theEmployee);
+
+    void deleteById(int theId);
 }
--- employeeCRUDApp/src/main/java/com/example/employeeCRUDApp/service/EmployeeServiceImpl.java
+++ employeeCRUDApp/src/main/java/com/example/employeeCRUDApp/service/EmployeeServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.employeeCRUDApp.service;
 
 import com.example.employeeCRUDApp.dao.EmployeeDao;
 import com.example.employeeCRUDApp.entity.Employee;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +21,22 @@ class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAl() {
         return employeeDao.findAll();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+        return employeeDao.findById(theId);
+    }
+
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDao.save(theEmployee);
+    }
+
+    @Transactional
+    @Override
+    public void deleteById(int theId) {
+        employeeDao.deleteById(theId);
     }
 }
